7b0423db60dfe3f6ab986815652a73b6d686596f
Mark Omer <markomerstudio@gmail.com>
2022-05-07 12:51:06 -0600

    update 20220507

--- config/settings.py
+++ config/settings.py
@@ -41,14 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'accounts',
     'pages',
     'posts',
-    'static',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,8 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -134,3 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
--- posts/views.py
+++ posts/views.py
@@ -6,7 +6,8 @@ from django.views.generic.edit import (
 )
 
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+  LoginRequiredMixin, UserPassesTestMixin)
 from django.urls import reverse_lazy
 
 
@@ -29,13 +30,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     return super().form_valid(form)
     
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   template_name = 'posts/edit.html'
   model = Post
   fields = ['title', 'body']
 
+  def test_func(self):
+    obj = self.get_object()
+    return obj.authtor == self.requst.user
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
   template_name = 'posts/delete.html'
   model = Post
   success_url = reverse_lazy('post_list')
+
+  def test_func(self):
+    obj = self.get_object()
+    return obj.authtor == self.requst.user
